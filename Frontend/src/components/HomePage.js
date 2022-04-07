a8de5ff34f6dfa397a890c204018aad43a9c9918
amir <amir.kelman@mail.huji.ac.il>
2022-04-07 18:56:31 +0300

    merge

--- Frontend/src/components/HomePage.js
+++ Frontend/src/components/HomePage.js
@@ -1,11 +1,8 @@
 import React from "react";
 import Map from "./Map";
 import Filters from "./Filters";
-<<<<<<< HEAD
 import Suggestions from "./Suggestions";
-=======
 import NestedList from "./NestedList";
->>>>>>> 70b868e3a3842ffcf1470fd9cbb7011fb88c5533
 const HomePage = () => {
     return (
         <div>
