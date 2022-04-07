6f5d5e1650a42e5efc7b17a51e4418100a204729
sugaryoni <yonatan.sugarmen@mail.huji.ac.il>
2022-04-07 19:02:43 +0300

    Update HomePage.js

--- Frontend/src/components/HomePage.js
+++ Frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Map from "./Map";
 import Filters from "./Filters";
+import Suggestions from "./Suggestions";
 import NestedList from "./NestedList";
 const HomePage = () => {
     return (
