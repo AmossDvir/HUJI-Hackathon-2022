8719d1291a7e713619ce98b421cc3d91eb6f9edf
amir <amir.kelman@mail.huji.ac.il>
2022-04-07 18:50:57 +0300

    add suggestions

--- Frontend/src/components/HomePage.js
+++ Frontend/src/components/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Map from "./Map";
 import Filters from "./Filters";
+import Suggestions from "./Suggestions";
 const HomePage = () => {
     return (
         <div>
             <Filters></Filters>
+            <Suggestions></Suggestions>
 
         </div>
     )
